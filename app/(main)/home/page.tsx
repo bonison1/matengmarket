7da6612b4f7fe6e3edc6849141648ac538ee62dd
bonison01 <164063307+bonison01@users.noreply.github.com>
2025-04-16 06:23:13 +0530

    dfs

--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -111,7 +111,7 @@ export default function page() {
               </span>
             </button>
           </Link>
-          <Link href={`https://matengbusiness.vercel.app/delivery_orders`}>
+          {/* <Link href={`https://matengbusiness.vercel.app/delivery_orders`}>
             <button
               className="relative inline-flex items-center gap-2 px-8 py-3 font-semibold text-teal-50 bg-gradient-to-tr from-teal-900/30 via-teal-900/70 to-teal-900/30 ring-4 ring-teal-900/20 rounded-full overflow-hidden hover:opacity-90 transition-opacity before:absolute before:top-4 before:left-1/2 before:-translate-x-1/2 before:w-[100px] before:h-[100px] before:rounded-full before:bg-gradient-to-b before:from-teal-50/10 before:blur-xl text-base animate-arrow"
             >
@@ -131,7 +131,7 @@ export default function page() {
                 </svg>
               </span>
             </button>
-          </Link>
+          </Link> */}
         </div>
 
         <div>
