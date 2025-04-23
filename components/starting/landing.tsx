b5e82e3f1e5ab5a9b5fdd3271e96c3467b180cac
bonison01 <164063307+bonison01@users.noreply.github.com>
2025-04-23 08:55:12 +0530

    dsf

--- components/starting/landing.tsx
+++ components/starting/landing.tsx
@@ -9,42 +9,42 @@ export default function Landing() {
 
   const { setTheme } = useTheme()
 
-    useEffect(() => {
-        setTheme("dark")
-    },[])
+  useEffect(() => {
+    setTheme("dark")
+  }, [])
 
   const text = "mateng";
   const [finalAnimation, setFinalAnimation] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
-    setFinalAnimation(false); 
+    setFinalAnimation(false);
 
     const timer = setTimeout(() => {
       setFinalAnimation(true);
 
       setTimeout(() => {
-        router.push("/home"); 
+        router.push("/home");
       }, 1500);
     }, 2000);
 
     return () => clearTimeout(timer);
   }, [router]);
 
-  return (
-    <div className="landing flex flex-col items-center justify-center w-[100vw] h-[90vh] sm:h-[100vh]">
-      <div className={`relative transition-all ${finalAnimation ? "logo-move" : ""}`}>
-        <div className={`logo ${finalAnimation ? "shrink" : ""}`}>
-          {text.split("").map((char, index) => (
-            <span key={index} className="letter" style={{ animationDelay: `${index * 0.2}s` }}>
-              {char}
-            </span>
-          ))}
-        </div>
-        <div className={`tagline absolute bottom-0 opacity-0 animate-fade-in ${finalAnimation ? "tagline-move" : ""}`}>
-          meeting people's needs
-        </div>
-      </div>
-    </div>
-  );
+  // return (
+  //   <div className="landing flex flex-col items-center justify-center w-[100vw] h-[90vh] sm:h-[100vh]">
+  //     <div className={`relative transition-all ${finalAnimation ? "logo-move" : ""}`}>
+  //       <div className={`logo ${finalAnimation ? "shrink" : ""}`}>
+  //         {text.split("").map((char, index) => (
+  //           <span key={index} className="letter" style={{ animationDelay: `${index * 0.2}s` }}>
+  //             {char}
+  //           </span>
+  //         ))}
+  //       </div>
+  //       <div className={`tagline absolute bottom-0 opacity-0 animate-fade-in ${finalAnimation ? "tagline-move" : ""}`}>
+  //         meeting people's needs
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 }
